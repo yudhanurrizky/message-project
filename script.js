2058410a78489932dd56a5aa2cad422981ad8d93
Yudha Nurrizky <89990254+yudhanurrizky@users.noreply.github.com>
2021-10-19 13:07:53 +0700

    Added wise quotes

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ function generateRandomNumber (num) {
     return Math.floor(Math.random() * num)
 };
 
+const collectiveMessages = {
+    wiseQuotes: ['He who has a why to live can bare any how. - Friedrich Nietzche', 
+                'Change is the law of life. And those who look only to the past or present are certain to miss the future. - John F. Kennedy',
+                'All life is an experiment. The more experiments you make the better. - Ralph Waldo Emerson',
+                'The only true wisdom is in knowing you know nothing. - Socrates',
+                'Any fool can know. The point is to understand. - Albert Einstein',
+                'The greatest glory in living lies not in never falling, but in rising every time we fall. - Nelson Mandela',
+                'The way to get started is to quit talking and begin doing. - Walt Disney']
+};
